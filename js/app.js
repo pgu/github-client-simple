d416a29b4d4bc6dd535a10d2669340a4c346f456
pgu <guilcher.pascal@gmail.com>
2014-05-20 21:02:22 +0200

    get 100 latest commits

--- js/app.js
+++ js/app.js
@@ -3,17 +3,8 @@
 angular.module('githubClient', ['ui.bootstrap'])
 
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
-    $scope.searchProject = function (val) {
-
-      if (_(val).size() < 3) {
-        return [];
-      }
+    $scope.searchProject = function (query) {
 
       var url = 'https://api.github.com/search/repositories';
 
@@ -22,7 +13,7 @@ angular.module('githubClient', ['ui.bootstrap'])
       };
 
       var params = {
-        q: val,
+        q: query,
         sort: 'stars',
         order: 'desc'
       };
@@ -30,10 +21,7 @@ angular.module('githubClient', ['ui.bootstrap'])
       return $http.get(url, { params: params, headers: headers })
         .then(function (res) {
           var response = res.data;
-
-          return _.map(response.items, function (item) {
-            return item.full_name;
-          });
+          return response.items;
         });
     };
 
@@ -46,13 +34,80 @@ angular.module('githubClient', ['ui.bootstrap'])
         Accept: 'application/vnd.github.v3+json'
       };
       return $http.get(url, { headers: headers })
-        .then(function(res) {
+        .then(function (res) {
           var response = res.data;
           $scope.contributors = response;
         });
 
     };
 
+    $scope.commits = [];
+    $scope.isFetchingCommits = false;
+    var COMMITS_LIMIT = 100;
+
+    function fetchCommitsUntil100 (commitsUrl) {
+
+      var headers = {
+        Accept: 'application/vnd.github.v3+json'
+      };
+
+      return $http.get(commitsUrl, { headers: headers })
+        .then(function (response) {
+
+          var commits = response.data;
+
+          if (_(commits).isEmpty()) {
+            return;
+          }
+
+          $scope.commits = $scope.commits.concat(commits);
+
+          var hasReachedLimit = _($scope.commits).size() >= COMMITS_LIMIT;
+          if (hasReachedLimit) {
+            return;
+          }
+
+          var headers = response.headers();
+
+          //  Header Link: <https://api.github.com/repositories/4991291/commits?page=2>; rel="next"
+          var noNextPage = !_.has(headers, 'link') ||
+            headers.link.indexOf('rel="next"') === -1;
+
+          if (noNextPage) {
+            return;
+          }
+
+          var links = headers.link.split(', ');
+          var nextLink = _.find(links, function (link) {
+            return link.indexOf('rel="next"') !== -1;
+          });
+
+          var fmtUrl = _(nextLink.split(';')).first();
+          var urlNextPage = fmtUrl.replace(/<|>/g, '');
+
+          return fetchCommitsUntil100(urlNextPage);
+
+        });
+    }
+
+    $scope.fetchLatest100Commits = function (selectedRepo) {
+      $scope.isFetchingCommits = true;
+      $scope.commits = [];
+
+      //  https://developer.github.com/v3/repos/commits/
+      //  GET /repos/:owner/:repo/commits
+      var commitsUrl = selectedRepo.commits_url.replace('{/sha}', '');
+
+      fetchCommitsUntil100(commitsUrl)
+        .finally(function () {
+          $scope.isFetchingCommits = false;
+        })
+      ;
+    };
+
+    $scope.noop = function (selected) {
+      console.info('> ', selected);
+    }
 
   });
 
